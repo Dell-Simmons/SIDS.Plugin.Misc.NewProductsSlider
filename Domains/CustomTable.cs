d8deaaa905193cd7c3e0c85b86dd3cbadf1c7bc3
Dell-Simmons <dell.simmons@gmail.com>
2023-07-29 09:51:06 -0700

    Added NewProductsSlider Plugin

---
+++ Domains/CustomTable.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Core;
+
+namespace SIDS.Plugin.Misc.NewProductsSlider.Domains
+{
+    public partial class CustomTable : BaseEntity
+    {
+
+    }
+}
---
+++ Mapping/Builders/PluginBuilder.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Data.Mapping.Builders;
+using SIDS.Plugin.Misc.NewProductsSlider.Domains;
+
+namespace SIDS.Plugin.Misc.NewProductsSlider.Mapping.Builders
+{
+    public class PluginBuilder : NopEntityBuilder<CustomTable>
+    {
+        #region Methods
+
+        public override void MapEntity(CreateTableExpressionBuilder table)
+        {
+        }
+
+        #endregion
+    }
+}
